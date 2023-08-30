a90f2e2f8c8064f26eefa8d190f8d711fa5db37e
Brianna Smith <briannanicole271@gmail.com>
2023-08-30 11:26:28 -0400

    Loop exercises almost completed because I am a rockstar

---
+++ main.js
@@ -0,0 +1,10 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+for(var i = 0; i < cohortOneStudents.length; i++){
+console.log(cohortOneStudents[i])
+}
+
+var myInterests = ["Traveling", "Web Browsing", "Relaxing", "Shopping", "Nail Care"]
+for (var i = 0; i < myInterests.length; i++){
+console.log(`One of my interests is: ${myInterests[i]}`)
+}
+
